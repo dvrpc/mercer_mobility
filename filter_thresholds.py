4d8c22cd95dfbe2c40b8781cd7f0f3ca5aba600e
Mark Morley <mmorley0395@gmail.com>
2022-11-29 15:43:38 -0500

    deleted filter_thresholds
    not needed anymore

--- filter_thresholds.py
+++
@@ -1,5 +0,0 @@
-# pseudocode
-
-# grab all layers but only within x buffer of mercer roadways, update views to this reality
-
-# find overlap between both AM periods, and both PM periods, as well as between all, create that as a view
