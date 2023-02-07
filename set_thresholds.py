106075c85f8940d16efbe3e4b255f4b09a5adf03
Mark Morley <mmorley0395@gmail.com>
2023-02-07 16:21:42 -0500

    trimmed unused thresholds out of script

--- set_thresholds.py
+++ set_thresholds.py
@@ -14,12 +14,14 @@ def create_threshold_view(
     and_or_statement: str = "",
     # append_to_views: bool = True,
 ):
-    """creates a view in postgres based on a threshold
+    """creates a view in postgres based on a threshold, to reduce the number of features you have to conflate later. basically setting a floor.
+
+        this was not done for all layers, only those where setting a floor is acceptable. other layers needed all features to be conflated.
 
     :param viewname: what you'd like the view to be called
     :param table: tablename you're selecting from
     :param column: column you're using for a threshold
-    :param sign: > < or =
+    :param sign: >=< or =
     :param threshold: threshold you want to set for a layer
     :param and_or_statement: if you want to add conditional and/or statements to base query
     :param append_to_views: only change if you want to later clip to road buffer (vs joining based on columns)"""
@@ -33,43 +35,7 @@ def create_threshold_view(
     views.append("view_" + viewname)
 
 
-def clip_to_mercerroads(view, line_buffer: float):
-    """clips segments in "views list" to a small buffer around mercer roads"""
-    db.execute(
-        f"""drop table if exists {view}_clipped;
-            create table {view}_clipped as(
-            with buffered as (
-                select st_buffer(geom, {line_buffer}) as geom from mercercountyjurisdictionroads_frommercer
-                )
-                select a.* from public.{view} a 
-                inner join buffered b 
-                on st_intersects(a.geom, b.geom)
-                )"""
-    )
-    print(f"creating clipped table for {view} view...")
-
-
 def set_thresholds():
-
-    # bridges
-    create_threshold_view(
-        f"bridges_1pt",
-        "bridges_joined",
-        "unofficial_sufficiency_rating",
-        "<=",
-        50,
-        "and unofficial_sufficiency_rating > 20",
-    )
-    create_threshold_view(
-        f"bridges_2pt", "bridges_joined", "unofficial_sufficiency_rating", "<=", 20
-    )
-
-    # pavement
-    create_threshold_view(
-        f"pavement1point", "pavement_evaluation", "pci", "<=", 60, "and pci > 30"
-    )
-    create_threshold_view(f"pavement2point", "pavement_evaluation", "pci", "<=", 30)
-
     # model data
     for val in [85, 100]:
         for period in ["model_vols2025_am_link", "model_vols2025_pm_link"]:
@@ -80,55 +46,15 @@ def set_thresholds():
                 ">",
                 val,
             )
-    # sidewalk
-    create_threshold_view("sw_gaps", "sidewalk_gaps_clipped", "sw_ratio", "<", ".5")
 
-    # crashes
-    create_threshold_view(
-        "vulnerable_user_crashes",
-        "safety_voyager",
-        "pedestrian_involved",
-        ">",
-        0,
-        "or cyclist_involved > 0",
-    )
-
-    create_threshold_view(
-        "ksi",
-        "safety_voyager",
-        "severity_rating_code",
-        "=",
-        "'Fatal Injury'",
-        "or severity_rating_code = 'Suspected Serious Injury'",
-    )
-
-    create_threshold_view(
-        "crashrate_seg_1_sd",
-        "crash_statistics_by_segment_mc",
-        "crrate",
-        ">=",
-        1256,  # st dev is 769. mean is 487. total is 1256.
-        "and crrate < 2025"
-        # append_to_views=False,
-    )
-
-    create_threshold_view(
-        "crashrate_seg_2_sd",
-        "crash_statistics_by_segment_mc",
-        "crrate",
-        ">=",
-        2025,  # 1256 (above) plus another st dev
-        # append_to_views=False,
-    )
     # pti/tti
-    # for timeperiod in ["0809", "0910", "1617", "1718"]:
     create_threshold_view(
         f"tti_all",
         "dvrpcnj_inrixxdgeo22_1_jointraveltime1min_mercer",
         "ttiwkd0708",
         ">=",
         1.2,
-        "or ttiwkd0809 > 1.2 or ttiwkd1617 > 1.2 or ttiwkd1718 > 1.2",
+        "or ttiwkd0809 >=1.2 or ttiwkd1617 >=1.2 or ttiwkd1718 >=1.2",
     )
 
     create_threshold_view(
@@ -137,12 +63,11 @@ def set_thresholds():
         f"ptiwkd0708",
         ">=",
         2,
-        "or ptiwkd0809 > 2 or ptiwkd1617 > 2 or ptiwkd1718 > 2",
+        "or ptiwkd0809 >=2 or ptiwkd1617 >=2 or ptiwkd1718 >=2",
     )
 
     print("thresholds set")
 
 
 set_thresholds()
-# for view in views:
-#     clip_to_mercerroads(view, 15.24)  # 15.24 is meters, == 50'
+
