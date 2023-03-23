fd04c3ab8abc99f274637d3e11cc83e7183c5ae1
Mark Morley <mmorley0395@gmail.com>
2023-03-23 13:20:22 -0400

    updated point assignment for crash ranking

--- assign_pts.py
+++ assign_pts.py
@@ -7,75 +7,40 @@ api_key = os.getenv("api_key")
 
 db = Database.from_config("mercer", "omad")
 
-
-def megajoin():
-    query = """
-    drop schema if exists point_assignment CASCADE;
-    create schema point_assignment;
-    create table point_assignment.megajoin as (
-        select 
-            a.*, 
-            b.inrixxd, 
-            b.ampmdelay as bottleneckvehdelay,
-            c.lsad_type
-        from rejoined.all a
-            left join public.bottlenecksvehvoldelay b
-                on st_within(b.geom, st_buffer(a.geom, 10))
-            left join public.uza c 
-                on st_within(a.geom, c.geom));
-    """
-    print("creating base scenario")
-    db.execute(query)
-
-
-def create_point_cols():
-    deficiencies = [
-        "vulusercrrate",
-        "ksicrrate",
-        "crrate",
-        "sidewalk",
-        "missing_bike_fac",
-        "transit_rt",
-        "tti",
-        "pti",
-        "bottleneck",
-        "vehvoldelay",
-    ]
-    col_names = []
-    for value in deficiencies:
-        v2 = value + "_pts"
-        col_names.append(v2)
-
-    for column in col_names:
+def rank_crashes():
+    rates = ['crrate', 'ksicrrate', 'vulcrrate']
+    for rate in rates:
         query = f"""
-            alter table point_assignment.megajoin drop column if exists {column};
-            alter table point_assignment.megajoin add column {column} integer;
-            update point_assignment.megajoin set {column} = 0 where {column} is null;"""
+            alter table rejoined.all add column if not exists {rate}_pts int;
+
+            --0-20%
+            update rejoined.all
+            set {rate}_pts = 1 where {rate} between 0 and (select max({rate})/5 from rejoined.all);
+
+            --21-40%
+            update rejoined.all
+            set {rate}_pts = 2 where {rate} between (select max({rate})/5 from rejoined.all) and ((select max({rate})/5 from rejoined.all) * 2);
+
+            --41-60%
+            update rejoined.all
+            set {rate}_pts = 3 where {rate} between ((select max({rate})/5 from rejoined.all) * 2) and ((select max({rate})/5 from rejoined.all) * 3);
+
+            --60-80%
+            update rejoined.all
+            set {rate}_pts = 4 where {rate} between ((select max({rate})/5 from rejoined.all) * 3) and  ((select max({rate})/5 from rejoined.all) * 4);
+
+
+            --80-100%
+            update rejoined.all
+            set {rate}_pts = 5 where {rate} between  ((select max({rate})/5 from rejoined.all) * 4) and ((select max({rate})/5 from rejoined.all) * 5);
+            """
         db.execute(query)
 
+    query = """alter table rejoined.all add column if not exists crash_pt_totals int;
+                update rejoined.all set crash_pt_totals = crrate_pts + ksicrrate_pts + vulcrrate_pts; """
 
-def assign_points(table: str, point_col: str, point: int, where_statement: str):
-    query = f"""
-    UPDATE point_assignment.{table} SET {point_col} = {point} WHERE {where_statement}
-    """
     db.execute(query)
 
-
-def total_points(table: str):
-    query = f"""
-    alter table point_assignment.{table} add column if not exists total int;
-    UPDATE point_assignment.{table} set total = vulusercrrate_pts + vc_pts + ksicrrate_pts + crrate_pts + sidewalk_pts + missing_bike_fac_pts + tti_pts + pti_pts + bottleneck_pts + transit_rt_pts + vehvoldelay_pts;
-    """
-    db.execute(query)
-
-
-def avg_and_sd(column: str, table: str):
-    """returns average and sd, calculated in pg"""
-    avg = db.query_as_singleton(f"select avg({column}) from point_assignment.{table}")
-    sd = db.query_as_singleton(f"select stddev({column}) from point_assignment.{table}")
-    return [avg, sd]
-
-
+    
 if __name__ == "__main__":
-    megajoin()
-    create_point_cols()
+    rank_crashes()
