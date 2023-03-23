1669d4181bcb7761ab1dbf6d1beda0fd385000ca
Mark Morley <mmorley0395@gmail.com>
2023-03-23 13:26:14 -0400

    filtered for county road when assigning points

--- assign_pts.py
+++ assign_pts.py
@@ -15,24 +15,24 @@ def rank_crashes():
 
             --0-20%
             update rejoined.all
-            set {rate}_pts = 1 where {rate} between 0 and (select max({rate})/5 from rejoined.all);
+            set {rate}_pts = 1 where {rate} between 0 and (select max({rate})/5 from rejoined.all where countyrd = 'County');
 
             --21-40%
             update rejoined.all
-            set {rate}_pts = 2 where {rate} between (select max({rate})/5 from rejoined.all) and ((select max({rate})/5 from rejoined.all) * 2);
+            set {rate}_pts = 2 where {rate} between (select max({rate})/5 from rejoined.all where countyrd = 'County') and ((select max({rate})/5 from rejoined.all where countyrd = 'County') * 2);
 
             --41-60%
             update rejoined.all
-            set {rate}_pts = 3 where {rate} between ((select max({rate})/5 from rejoined.all) * 2) and ((select max({rate})/5 from rejoined.all) * 3);
+            set {rate}_pts = 3 where {rate} between ((select max({rate})/5 from rejoined.all where countyrd = 'County') * 2) and ((select max({rate})/5 from rejoined.all where countyrd = 'County') * 3);
 
             --60-80%
             update rejoined.all
-            set {rate}_pts = 4 where {rate} between ((select max({rate})/5 from rejoined.all) * 3) and  ((select max({rate})/5 from rejoined.all) * 4);
+            set {rate}_pts = 4 where {rate} between ((select max({rate})/5 from rejoined.all where countyrd = 'County') * 3) and  ((select max({rate})/5 from rejoined.all where countyrd = 'County') * 4);
 
 
             --80-100%
             update rejoined.all
-            set {rate}_pts = 5 where {rate} between  ((select max({rate})/5 from rejoined.all) * 4) and ((select max({rate})/5 from rejoined.all) * 5);
+            set {rate}_pts = 5 where {rate} between  ((select max({rate})/5 from rejoined.all where countyrd = 'County') * 4) and ((select max({rate})/5 from rejoined.all where countyrd = 'County') * 5);
             """
         db.execute(query)
 
