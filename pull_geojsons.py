423a3ae00fd36018c368f6c2d1571855b8471e7d
Mark Morley <mmorley0395@gmail.com>
2023-03-24 15:56:55 -0400

    reformatted geojson export in ogr

--- pull_geojsons.py
+++ pull_geojsons.py
@@ -2,7 +2,6 @@ from pg_data_etl import Database
 import os
 from dotenv import load_dotenv
 from pathlib import Path
-from assign_pts import scenarios
 
 load_dotenv()
 
@@ -11,14 +10,20 @@ data_folder = Path(os.getenv("data_root"))  # path to g drive folder'
 outputpath = data_folder / "Outputs" / "GeoJSON"
 
 
-def create_and_export_geojson(scenario: str):
+def create_and_export_geojson(
+    schema: str,
+    pg_tablename: str,
+):
     os.system(
-        f"""ogr2ogr -f GeoJSON '{outputpath}'/scenario_{scenario}.geojson  -lco -overwrite PG:"host={db.connection_params['host']} port={db.connection_params['port']} user={db.connection_params['un']} dbname={db.connection_params['db_name']} password={db.connection_params['pw']}" -sql "select * from point_assignment.scenario_{scenario}" 
-                """
+        f"ogr2ogr -f GeoJSON '{outputpath}'/{pg_tablename}.geojson "
+        + f"-lco -overwrite PG:'host={db.connection_params['host']} "
+        + f"port={db.connection_params['port']} "
+        + f"user={db.connection_params['un']} "
+        + f"dbname={db.connection_params['db_name']} "
+        + f"password={db.connection_params['pw']}' -sql 'select * from {schema}.{pg_tablename}'"
     )
 
 
 if __name__ == "__main__":
-    for scenario in scenarios:
-        print(f"working on scenario_{scenario}")
-        create_and_export_geojson(scenario)
+    create_and_export_geojson("rejoined", "all")
+    create_and_export_geojson("public", "high_priority")
